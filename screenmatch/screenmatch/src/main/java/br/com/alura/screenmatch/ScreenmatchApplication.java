9b3cc6bc714a93fe61875bb079a3c2ef11baa771
leviutima <levi.utima@gmail.com>
2025-06-27 18:49:25 -0300

    feat: mapper temporadas and desserealize json

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,15 @@
 package br.com.alura.screenmatch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSeries;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ApiConsume;
 import br.com.alura.screenmatch.service.ConverterDados;
 
@@ -28,6 +32,14 @@ public class ScreenmatchApplication implements CommandLineRunner{
 		DadosEpisodio dadosEpisodio = conversor.obterDados(jsonEpisode, DadosEpisodio.class);
 		System.err.println(dadosEpisodio);
 
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dadosSeries.temporadas(); i++) {
+			var jsonTemporadas = apiConsume.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporadas, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		System.out.println(temporadas);
 	}
 
 }
