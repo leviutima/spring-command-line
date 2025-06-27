0ae0338b91a27120bfca0d2335eaf3642a37e06c
leviutima <levi.utima@gmail.com>
2025-06-27 17:21:12 -0300

    feat: consume api

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.service.ApiConsume;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
 
@@ -13,7 +15,11 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("Primeiro projeto spring sem web");
+		var apiConsume = new ApiConsume();
+		var json = apiConsume.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.err.println(json);
+		json = apiConsume.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.err.println(json);
 	}
 
 }
