673cdbdc588bba7bf179746f3851f1c473af3463
leviutima <levi.utima@gmail.com>
2025-06-28 00:02:47 -0300

    feat: implement stream in array

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/PrincipalAlura.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/PrincipalAlura.java
@@ -1,11 +1,9 @@
 package br.com.alura.screenmatch.principal;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import br.com.alura.screenmatch.model.DadosSeries;
-import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ApiConsume;
 import br.com.alura.screenmatch.service.ConverterDados;
 
@@ -19,29 +17,33 @@ public class PrincipalAlura {
     private final String API_KEY = "&apikey=6585022c";
 
     public void exibeMenu() {
-        System.err.println("Digite o nome da série para busca");
-        var nomeSerie = leitura.nextLine();
-        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        DadosSeries dadosSeries = conversor.obterDados(json, DadosSeries.class);
-        System.err.println(dadosSeries);
+        // System.err.println("Digite o nome da série para busca");
+        // var nomeSerie = leitura.nextLine();
+        // var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        // DadosSeries dadosSeries = conversor.obterDados(json, DadosSeries.class);
+        // System.err.println(dadosSeries);
 
-        List<DadosTemporada> temporadas = new ArrayList<>();
+        // List<DadosTemporada> temporadas = new ArrayList<>();
 
 
-        for (int i = 1; i <= dadosSeries.temporadas(); i++) {
-            var jsonTemporadas = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporadas, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-        }
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-
-        // for( int i = 0; i < dadosSeries.temporadas(); i++) {
-        //     List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-        //     for(int j = 0; j < episodiosTemporada.size(); j++) {
-        //         System.err.println(episodiosTemporada.get(j).titulo());
-        //     }
+        // for (int i = 1; i <= dadosSeries.temporadas(); i++) {
+        //     var jsonTemporadas = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+        //     DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporadas, DadosTemporada.class);
+        //     temporadas.add(dadosTemporada);
         // }
+        // temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        
 
+        // // for( int i = 0; i < dadosSeries.temporadas(); i++) {
+        // //     List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+        // //     for(int j = 0; j < episodiosTemporada.size(); j++) {
+        // //         System.err.println(episodiosTemporada.get(j).titulo());
+        // //     }
+        // // }
+
+        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Levi");
+        nomes.stream().sorted().limit(3).forEach(System.out::println);
+               
         
     }
 }
--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverterDados.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverterDados.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverterDados implements IConvertDados {
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
